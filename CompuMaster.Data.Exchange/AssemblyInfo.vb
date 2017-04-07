5379334a83711567805e7b93423165e23596d983
Jochen Wezel <jwezel@compumaster.de>
2017-04-07 19:35:40 +0200

    re-published NuGet package

--- CompuMaster.Data.Exchange/AssemblyInfo.vb
+++ CompuMaster.Data.Exchange/AssemblyInfo.vb
@@ -6,10 +6,10 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("CompuMaster GmbH")> 
 <Assembly: AssemblyProduct("CompuMaster.Data.Exchange2007")>
-<Assembly: AssemblyCopyright("2008-2014,2016 CompuMaster GmbH")>
+<Assembly: AssemblyCopyright("2008-2014,2016,2017 CompuMaster GmbH")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(True)> 
 <Assembly: Guid("9b16fd1c-a4c3-410f-ba6c-230cceee31b5")>
 
-<Assembly: AssemblyVersion("2016.09.07.100")>
+<Assembly: AssemblyVersion("2017.04.07.101")>
